b86b6263447438301737503a16120b596649d2d9
Georg Veichtlbauer <georg@vware.at>
2020-12-15 22:11:07 +0100

    castor: update blob list & vendor setup scripts
    Change-Id: I1b93df29bd49895f18f123328571a8534d125d10

--- extract-files.sh
+++ extract-files.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Use tradition sort
-export LC_ALL=C
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
-FP=$(cd ${0%/*} && pwd -P)
-export VENDOR=$(basename $(dirname $FP))
-export DEVICE=$(basename $FP)
-export BOARDCONFIGVENDOR=true
+set -e
 
-../common/extract-files.sh $@
+export DEVICE=castor
+export DEVICE_COMMON=shinano-common
+export VENDOR=sony
 
-../common/setup-makefiles.sh
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
