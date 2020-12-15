355c157130d31709d9571810c5b169ef779215cf
Georg Veichtlbauer <georg@vware.at>
2020-12-15 22:11:07 +0100

    castor: update blob list & vendor setup scripts
    Change-Id: I1b93df29bd49895f18f123328571a8534d125d10

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,25 +1,30 @@
-# Copyright (C) 2014 The CyanogenMod Project
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# inherit from the common castor definitions
-include device/sony/castor_windy/BoardConfigCommon.mk
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := SGP521,SGP541,SGP551,castor
+set -e
 
-# Kernel properties
-TARGET_KERNEL_CONFIG := lineageos_shinano_castor_defconfig
+export DEVICE=castor
+export DEVICE_COMMON=shinano-common
+export VENDOR=sony
 
-# Inherit from proprietary files
-include vendor/sony/castor/BoardConfigVendor.mk
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
